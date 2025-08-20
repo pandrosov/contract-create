f1193364a2809d1811845a83e0bb68e87021fd8d
pandrosov <pandrosov@list.ru>
2025-08-20 14:15:34 +0300

    Fix: remove model imports to prevent circular dependencies

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,20 +1,2 @@
-# Импорты всех моделей для правильной инициализации базы данных
-from .user import User
-from .folder import Folder
-from .template import Template
-from .permission import Permission
-from .action_log import ActionLog
-from .settings import Settings
-from .placeholder_description import PlaceholderDescription
-from .log import Log
-
-__all__ = [
-    "User",
-    "Folder", 
-    "Template",
-    "Permission",
-    "ActionLog",
-    "Settings",
-    "PlaceholderDescription",
-    "Log"
-] 
+# Модели импортируются по мере необходимости
+# Это предотвращает циклические импорты 
